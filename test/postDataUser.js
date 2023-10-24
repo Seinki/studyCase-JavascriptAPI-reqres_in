8416c1be8f89da4b8972e02dfb6994a5bda1fb9a
Seinki <121332397+Seinki@users.noreply.github.com>
2023-10-24 21:22:03 +0700

    update constant for data user

--- test/postDataUser.js
+++ test/postDataUser.js
@@ -1,12 +1,10 @@
 const request = require("supertest");
 const server = require("../env");
+const DATA = require('../data/userData')
 
 describe("Post Request for Create User", () => {
   it("Success create user", async () => {
-    const response = request(server()).post("api/users").send({
-      name: "seinki",
-      job: "QA Engineer",
-    });
+    const response = request(server()).post("api/users").send(DATA.CREATE_USER_DATA);
     console.log((await response).status);
     console.log((await response).body);
   });
